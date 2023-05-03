4bbf4b9298f9205a09dc80eb153bcb1e76bb8312
ahmaddfauzi24 <wrk.ahmadfauzi@gmail.com>
2023-05-03 11:53:12 +0700

    First Commit and Public Assets

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderHB />
+    <HeroHB />
+    <ProductHB />
+    <IklanHB />
+    <FooterHB />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderHB from '@/components/HeaderHB.vue'
+import HeroHB from '@/components/HeroHB.vue'
+import ProductHB from '@/components/ProductHB.vue'
+import IklanHB from '@/components/IklanHB.vue'
+import FooterHB from '@/components/FooterHB.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+    HeaderHB,
+    HeroHB,
+    ProductHB,
+    IklanHB,
+    FooterHB
+    
+  }
+}
+</script>
